640f3ccfd2594abdb43af8ad024e5763a8fb578d
MagnusCarlsen26 <khushal.sindhav26@gmail.com>
2024-08-09 17:40:19 +0530

    Pushing to run on AWS.

--- comments.py
+++ comments.py
@@ -1,15 +1,20 @@
 import requests
 import json
 import csv
+import os
+
+def createFolder(folder_path):
+    os.makedirs(folder_path, exist_ok=True)
 
 def fetch_data(url, headers):
-  response = requests.get(url, headers=headers)
-  response.raise_for_status()
-  return response.json()
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
 
 
-def main(slug):
-
+def main(info):
+    print(info)
+    slug = info[1]
     page = 0
     url = f"https://www.practo.com/marketplace-api/dweb/profile/provider/feedback?slug={slug}&profile_type=PROVIDER&page={page}&mr=true&active_filter%5Bid%5D=0&active_filter%5Btext%5D=All&active_filter%5Btype%5D=All&show_recommended_reviews=true&show_feedback_summary_tags=true"
 
@@ -28,7 +33,7 @@ def main(slug):
         }
     data = fetch_data(url,headers)
 
-    with open('comment.json', 'w') as outfile:
+    with open(f'{info[2]}/{info[0]}.json', 'w') as outfile:
         json.dump(data, outfile, indent=4)
     print('Data successfully written to comment.json')
 
@@ -36,4 +41,4 @@ with open("fuck.csv","r",encoding="utf-8") as file :
     content = list(csv.reader(file))
     for i in range(0,len(content)) : 
        print(content[i][1])
-       main(content[i][1])
+       main(content[i])
